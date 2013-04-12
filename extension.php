7949ed6e8ade0f0313fe90c23e79328ffcb0b610
Peter Epp <peter@teknocat.org>
2013-04-11 21:19:14 -0600

    Updated to Biscuit 2.2 version

--- extension.php
+++ extension.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Extension for easy inclusion of table sorter JS
+ *
+ * @package Extensions
+ * @subpackage TableSort
+ * @author Peter Epp
+ * @copyright Copyright (c) 2009 Peter Epp (http://teknocat.org)
+ * @license GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.html)
+ * @version 2.0 $Id: extension.php 13982 2011-08-08 18:51:55Z teknocat $
+ */
 class TableSort extends AbstractExtension {
 	public function run() {
 		$this->register_js('footer','jquery.tablesorter.min.js');
@@ -6,4 +16,3 @@ class TableSort extends AbstractExtension {
 		$this->register_css(array('filename' => 'tablesort.css', 'media' => 'screen,projection,print'));
 	}
 }
-?>
